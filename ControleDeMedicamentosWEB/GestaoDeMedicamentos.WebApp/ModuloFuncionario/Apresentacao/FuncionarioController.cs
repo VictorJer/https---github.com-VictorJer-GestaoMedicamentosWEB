fec2ae8cc2bb58199aac44647d725d4831801f50
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-06-13 18:49:15 -0300

    ADD Cadastara Editar Excluir Funcionario Controller

--- ControleDeMedicamentosWEB/GestaoDeMedicamentos.WebApp/ModuloFuncionario/Apresentacao/FuncionarioController.cs
+++ ControleDeMedicamentosWEB/GestaoDeMedicamentos.WebApp/ModuloFuncionario/Apresentacao/FuncionarioController.cs
@@ -1,5 +1,6 @@
 using GestaoDeMedicamentos.WebApp.ModuloFuncionario.Apresentacao;
 using GestaoDeMedicamentos.WebApp.ModuloFuncionario.Dominio;
+using GestaoDeMedicamentos.WebApp.ModuloFuncionario.Infra;
 using Microsoft.AspNetCore.Mvc;
 
 public class FuncionarioController : Controller
@@ -31,5 +32,95 @@ public class FuncionarioController : Controller
         }
 
         return View(listarVm);
+    }
+
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        CadastrarFuncionarioViewModel cadastarVm = new CadastrarFuncionarioViewModel(
+            string.Empty,
+            string.Empty,
+            string.Empty
+        );
+
+        return View(cadastarVm);
+    }
+
+    [HttpPost]
+    public ActionResult Cadastrar(CadastrarFuncionarioViewModel cadastrarVm)
+    {
+        if (!ModelState.IsValid)
+            return View(cadastrarVm);
+
+        Funcionario novoFuncionario = new Funcionario(
+            cadastrarVm.Nome,
+            cadastrarVm.Telefone,
+            cadastrarVm.CPF
+        );
+
+        repositorioFuncionario.Cadastrar(novoFuncionario);
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Editar(string id)
+    {
+        Funcionario? funcionario = repositorioFuncionario.SelecionarPorId(id);
+
+        if (funcionario == null)
+            return RedirectToAction(nameof(Listar));
+
+        EditaFuncionarioViewModel editarVm = new EditaFuncionarioViewModel(
+            id,
+            funcionario.Nome,
+            funcionario.Telefone,
+            funcionario.CPF
+        );
+
+        return View(editarVm);
+    }
+
+    [HttpPost]
+    public ActionResult Editar(EditaFuncionarioViewModel editarVm)
+    {
+        if (!ModelState.IsValid)
+            return View(editarVm);
+
+        Funcionario funcionario = new Funcionario(
+            editarVm.Nome,
+            editarVm.Telefone,
+            editarVm.CPF
+        );
+
+        repositorioFuncionario.Editar(editarVm.Id, funcionario);
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Excluir(string id)
+    {
+        Funcionario? funcionario = repositorioFuncionario.SelecionarPorId(id);
+
+        if (funcionario == null)
+            return RedirectToAction(nameof(Listar));
+
+        ExcluirFuncionaruioviewModel excluirVm = new ExcluirFuncionaruioviewModel(
+            id,
+            funcionario.Nome,
+            funcionario.Telefone,
+            funcionario.CPF
+        );
+
+        return View(excluirVm);
+    }
+
+    [HttpPost]
+    public ActionResult Excluir(ExcluirFuncionaruioviewModel excluirVm)
+    {
+        repositorioFuncionario.Excluir(excluirVm.Id);
+
+        return RedirectToAction(nameof(Listar));
     }
 }
